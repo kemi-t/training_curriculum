5b529a05c1719d64c0eee60fb662b18ee6e27bb1
kemi-t <73261319+kemi-t@users.noreply.github.com>
2020-11-04 20:50:35 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -27,7 +27,6 @@ class CalendarsController < ApplicationController
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
-    youbi = ""
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
@@ -37,27 +36,12 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      case (@todays_date + x).wday
-      when 0
-        youbi = wdays[0]
-      when 1
-        youbi = wdays[1]
-      when 2
-        youbi = wdays[2]
-      when 3
-        youbi = wdays[3]
-      when 4
-        youbi = wdays[4]
-      when 5
-        youbi = wdays[5]
-      when 6
-        youbi = wdays[6]
-      end
+
 
       days = {
          month: (@todays_date + x).month,
          date: (@todays_date + x).day,
-         wday: youbi,
+         wday: wdays[(@todays_date+x).wday],
          plans: today_plans
         }
 
